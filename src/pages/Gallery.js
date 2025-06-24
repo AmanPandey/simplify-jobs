1663a074b9122457e253cef8f5a6964ed0a4c7a4
vijeta <vijetavarma1@gmail.com>
2025-06-24 12:52:18 +0530

    Gallery page added

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,11 +1,180 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Gallery = () => {
   return (
-    <div className="container py-5">
-      <h1>Gallery</h1>
-      <p>This page will display a collection of images or media related to your platform.</p>
-    </div>
+    <>
+      <section
+        className="section-hero overlay inner-page bg-image"
+        style={{ backgroundImage: "url('images/hero_1.jpg')" }}
+        id="home-section"
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-7">
+              <h1 className="text-white font-weight-bold">Gallery</h1>
+              <div className="custom-breadcrumbs">
+                <Link to="/">Home</Link> <span className="mx-2 slash">/</span>
+                <span className="text-white">
+                  <strong>Gallery</strong>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="site-section" id="next-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_1.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_1.jpg" />
+              </a>
+            </div>
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_2.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_2.jpg" />
+              </a>
+            </div>
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_3.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_3.jpg" />
+              </a>
+            </div>
+
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_4.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_4.jpg" />
+              </a>
+            </div>
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_5.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_5.jpg" />
+              </a>
+            </div>
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_6.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_6.jpg" />
+              </a>
+            </div>
+
+            <div className="col-md-6 col-lg-6 item">
+              <a
+                href="images/sq_img_11.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_11.jpg" />
+              </a>
+            </div>
+            <div className="col-md-6 col-lg-6 item">
+              <a
+                href="images/sq_img_2.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_2.jpg" />
+              </a>
+            </div>
+
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_7.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_7.jpg" />
+              </a>
+            </div>
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_8.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_8.jpg" />
+              </a>
+            </div>
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_9.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_9.jpg" />
+              </a>
+            </div>
+
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_10.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_10.jpg" />
+              </a>
+            </div>
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_11.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_11.jpg" />
+              </a>
+            </div>
+            <div className="col-md-6 col-lg-4 item">
+              <a
+                href="images/sq_img_12.jpg"
+                className="item-wrap fancybox"
+                data-fancybox="gallery2"
+              >
+                <span className="icon-search2"></span>
+                <img className="img-fluid" src="images/sq_img_12.jpg" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
