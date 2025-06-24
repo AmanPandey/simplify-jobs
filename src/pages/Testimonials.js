fd209b66293fdd55cd8255af1bbb0e35ee31ac54
vijeta <vijetavarma1@gmail.com>
2025-06-24 12:49:34 +0530

    Testimonial page added

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -1,11 +1,271 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Testimonials = () => {
   return (
-    <div className="container py-5">
-      <h1>Testimonials</h1>
-      <p>This page will display customer or client testimonials and feedback.</p>
-    </div>
+    <>
+      <section
+        className="section-hero overlay inner-page bg-image"
+        style={{ backgroundImage: "url('images/hero_1.jpg')" }}
+        id="home-section"
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-7">
+              <h1 className="text-white font-weight-bold">
+                Candidate's Testimonials
+              </h1>
+              <div className="custom-breadcrumbs">
+                <Link href="/">Home</Link> <span className="mx-2 slash">/</span>
+                <span className="text-white">
+                  <strong>Testimonials</strong>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="site-section" id="next-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 mb-4">
+              <div className="block__87154">
+                <blockquote>
+                  <p>
+                    &ldquo;Ipsum harum assumenda in eum vel eveniet numquam
+                    cumque vero vitae enim cupiditate deserunt eligendi officia
+                    modi consectetur. Expedita tempora quos nobis earum hic ex
+                    asperiores quisquam optio nostrum sit&rdquo;
+                  </p>
+                </blockquote>
+                <div className="block__91147 d-flex align-items-center">
+                  <figure className="mr-4">
+                    <img
+                      src="images/person_1.jpg"
+                      alt="Image"
+                      className="img-fluid"
+                    />
+                  </figure>
+                  <div>
+                    <h3>Elisabeth Smith</h3>
+                    <span className="position">Creative Director</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 mb-4">
+              <div className="block__87154">
+                <blockquote>
+                  <p>
+                    &ldquo;Ipsum harum assumenda in eum vel eveniet numquam,
+                    cumque vero vitae enim cupiditate deserunt eligendi officia
+                    modi consectetur. Expedita tempora quos nobis earum hic ex
+                    asperiores quisquam optio nostrum sit&rdquo;
+                  </p>
+                </blockquote>
+                <div className="block__91147 d-flex align-items-center">
+                  <figure className="mr-4">
+                    <img
+                      src="images/person_2.jpg"
+                      alt="Image"
+                      className="img-fluid"
+                    />
+                  </figure>
+                  <div>
+                    <h3>Chris Peter</h3>
+                    <span className="position">Web Designer</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 mb-4">
+              <div className="block__87154">
+                <blockquote>
+                  <p>
+                    &ldquo;Ipsum harum assumenda in eum vel eveniet numquam
+                    cumque vero vitae enim cupiditate deserunt eligendi officia
+                    modi consectetur. Expedita tempora quos nobis earum hic ex
+                    asperiores quisquam optio nostrum sit&rdquo;
+                  </p>
+                </blockquote>
+                <div className="block__91147 d-flex align-items-center">
+                  <figure className="mr-4">
+                    <img
+                      src="images/person_3.jpg"
+                      alt="Image"
+                      className="img-fluid"
+                    />
+                  </figure>
+                  <div>
+                    <h3>Benjamin Lewis</h3>
+                    <span className="position">Creative Director</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 mb-4">
+              <div className="block__87154">
+                <blockquote>
+                  <p>
+                    &ldquo;Ipsum harum assumenda in eum vel eveniet numquam,
+                    cumque vero vitae enim cupiditate deserunt eligendi officia
+                    modi consectetur. Expedita tempora quos nobis earum hic ex
+                    asperiores quisquam optio nostrum sit&rdquo;
+                  </p>
+                </blockquote>
+                <div className="block__91147 d-flex align-items-center">
+                  <figure className="mr-4">
+                    <img
+                      src="images/person_4.jpg"
+                      alt="Image"
+                      className="img-fluid"
+                    />
+                  </figure>
+                  <div>
+                    <h3>Pippa Cooper</h3>
+                    <span className="position">Web Designer</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-12 mb-4">
+              <div className="block__87154 bg-primary">
+                <blockquote>
+                  <p className="text-white">
+                    &ldquo;Ipsum harum assumenda in eum vel eveniet numquam,
+                    cumque vero vitae enim cupiditate deserunt eligendi officia
+                    modi consectetur. Expedita tempora quos nobis earum hic ex
+                    asperiores quisquam optio nostrum sit&rdquo;
+                  </p>
+                </blockquote>
+                <div className="block__91147 d-flex align-items-center">
+                  <figure className="mr-4">
+                    <img
+                      src="images/person_4.jpg"
+                      alt="Image"
+                      className="img-fluid"
+                    />
+                  </figure>
+                  <div>
+                    <h3 className="text-white">Pippa Cooper</h3>
+                    <span className="position position-2">Web Designer</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 mb-4">
+              <div className="block__87154">
+                <blockquote>
+                  <p>
+                    &ldquo;Ipsum harum assumenda in eum vel eveniet numquam
+                    cumque vero vitae enim cupiditate deserunt eligendi officia
+                    modi consectetur. Expedita tempora quos nobis earum hic ex
+                    asperiores quisquam optio nostrum sit&rdquo;
+                  </p>
+                </blockquote>
+                <div className="block__91147 d-flex align-items-center">
+                  <figure className="mr-4">
+                    <img
+                      src="images/person_1.jpg"
+                      alt="Image"
+                      className="img-fluid"
+                    />
+                  </figure>
+                  <div>
+                    <h3>Elisabeth Smith</h3>
+                    <span className="position">Creative Director</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 mb-4">
+              <div className="block__87154">
+                <blockquote>
+                  <p>
+                    &ldquo;Ipsum harum assumenda in eum vel eveniet numquam,
+                    cumque vero vitae enim cupiditate deserunt eligendi officia
+                    modi consectetur. Expedita tempora quos nobis earum hic ex
+                    asperiores quisquam optio nostrum sit&rdquo;
+                  </p>
+                </blockquote>
+                <div className="block__91147 d-flex align-items-center">
+                  <figure className="mr-4">
+                    <img
+                      src="images/person_2.jpg"
+                      alt="Image"
+                      className="img-fluid"
+                    />
+                  </figure>
+                  <div>
+                    <h3>Chris Peter</h3>
+                    <span className="position">Web Designer</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 mb-4">
+              <div className="block__87154">
+                <blockquote>
+                  <p>
+                    &ldquo;Ipsum harum assumenda in eum vel eveniet numquam
+                    cumque vero vitae enim cupiditate deserunt eligendi officia
+                    modi consectetur. Expedita tempora quos nobis earum hic ex
+                    asperiores quisquam optio nostrum sit&rdquo;
+                  </p>
+                </blockquote>
+                <div className="block__91147 d-flex align-items-center">
+                  <figure className="mr-4">
+                    <img
+                      src="images/person_3.jpg"
+                      alt="Image"
+                      className="img-fluid"
+                    />
+                  </figure>
+                  <div>
+                    <h3>Benjamin Lewis</h3>
+                    <span className="position">Creative Director</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-6 mb-4">
+              <div className="block__87154">
+                <blockquote>
+                  <p>
+                    &ldquo;Ipsum harum assumenda in eum vel eveniet numquam,
+                    cumque vero vitae enim cupiditate deserunt eligendi officia
+                    modi consectetur. Expedita tempora quos nobis earum hic ex
+                    asperiores quisquam optio nostrum sit&rdquo;
+                  </p>
+                </blockquote>
+                <div className="block__91147 d-flex align-items-center">
+                  <figure className="mr-4">
+                    <img
+                      src="images/person_4.jpg"
+                      alt="Image"
+                      className="img-fluid"
+                    />
+                  </figure>
+                  <div>
+                    <h3>Pippa Cooper</h3>
+                    <span className="position">Web Designer</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
